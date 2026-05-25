b086836dca48b3c0953919fcfece4ca365ee1912
harekrisna108nam-commits <harekrishna108nam@gmail.com>
2026-05-25 18:06:57 +0530

    updated

--- src/main/java/com/example/dhan_rsi_series/service/DhanAuthService.java
+++ src/main/java/com/example/dhan_rsi_series/service/DhanAuthService.java
@@ -184,7 +184,7 @@ public class DhanAuthService {
 //                    return new TokenData(accessToken, expiry);
 //                });
         
-        return Mono.just("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzc5NzYzMTY5LCJpYXQiOjE3Nzk2NzY3NjksInRva2VuQ29uc3VtZXJUeXBlIjoiU0VMRiIsIndlYmhvb2tVcmwiOiIiLCJkaGFuQ2xpZW50SWQiOiIxMTA2Mjg5ODAzIn0.WISOvpojuB5Mn4cbahSu9x4XpGsp2ph9VWTaDc8TmFsmdhqKjp-WYQl0czTOpmUaLtLsMUOtmTUlKnlda-hdlQ");
+        return Mono.just("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzc5Nzk4OTM5LCJpYXQiOjE3Nzk3MTI1MzksInRva2VuQ29uc3VtZXJUeXBlIjoiU0VMRiIsIndlYmhvb2tVcmwiOiIiLCJkaGFuQ2xpZW50SWQiOiIxMTA2Mjg5ODAzIn0.hIXDwlgS-q248a1mex6LZoIboU2R6L6R46kEL9b-3FblcKN3OjZGlQMcLGy0Y4LhWkKu-Lg2YlJlEkLgm0Qq1Q");
     }
     
  // 2️⃣ Renew Token
